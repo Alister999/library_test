17c57d87271da890d5753a5e697df88ff122c2c3
Aleksandr Sidorov <a.sidorov@teleguard.com>
2025-05-20 22:35:13 +0200

    add models of books and readers

---
+++ src/models/reader.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped
+from sqlalchemy.testing.schema import mapped_column
+from src.models.general import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, unique=True, index=True)
+    name: Mapped[str] = mapped_column(index=True, nullable=False)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False)
